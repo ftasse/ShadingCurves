7eb367a3172b10bd3bb27418e424f19a9bb66285
Henrik Lieng <hl391@cam.ac.uk>
2013-04-18 14:16:07 +0100

    hacky solution to equal values in DT close to curve

--- Curve/Surface.cpp
+++ Curve/Surface.cpp
@@ -320,7 +320,6 @@ QVector<QVector<int> > Surface::setSurfaceCP(QVector<ControlPoint> controlPoints
     for (int k=0; k<controlPoints.size(); ++k)
     {
         QPointF normal = normals[k];
-        qDebug() << "Normal, surface:" << normals[k].x() << normals[k].y();
         float extent = controlPoints[k].attribute(direction).extent;
         float height = controlPoints[k].attribute(direction).height;
         QVector<QPointF> shapeAtrs = controlPoints[k].attribute(direction).shapePointAtr;
@@ -431,7 +430,7 @@ QPointF Surface::traceDT(cv::Mat dt,QPoint current,float width,QLineF normalL)
         QPoint m = localMax(dt,cv::Rect(current.x()-1,current.y()-1,current.x()+1,current.y()+1)
                             ,&currentD,normalL);
         // check lines
-        if(fabs(oldD-currentD)<EPSILON || currentD >= width) {
+        if((fabs(oldD-currentD)<EPSILON&&currentD>1.1f) || currentD >= width) {
             new_cpt.rx() = m.rx();
             new_cpt.ry() = m.ry();
             break;
